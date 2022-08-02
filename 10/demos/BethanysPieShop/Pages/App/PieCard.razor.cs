3bfc85b2d7e410131a0a4cf6cb721932009bf036
Ayub254 <jobkarani084j@gmail.com>
2022-08-02 13:00:45 +0300

    initial commit

---
+++ 10/demos/BethanysPieShop/Pages/App/PieCard.razor.cs
@@ -0,0 +1,11 @@
+﻿using BethanysPieShop.Models;
+using Microsoft.AspNetCore.Components;
+
+namespace BethanysPieShop.Pages.App
+{
+    public partial class PieCard
+    {
+        [Parameter]
+        public Pie? Pie { get; set; }
+    }
+}
---
+++ 10/demos/BethanysPieShop/Program.cs
@@ -0,0 +1,58 @@
+using BethanysPieShop.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Text.Json.Serialization;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+    }); ;
+
+//builder.Services.AddScoped<ICategoryRepository, MockCategoryRepository>();
+//builder.Services.AddScoped<IPieRepository, MockPieRepository>();
+
+builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
+builder.Services.AddScoped<IPieRepository, PieRepository>();
+builder.Services.AddScoped<IOrderRepository, OrderRepository>();
+
+
+builder.Services.AddScoped<IShoppingCart, ShoppingCart>(sp => ShoppingCart.GetCart(sp));
+builder.Services.AddSession(); 
+builder.Services.AddHttpContextAccessor();
+
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+
+builder.Services.AddDbContext<BethanysPieShopDbContext>(options => {
+    options.UseSqlServer(
+        builder.Configuration["ConnectionStrings:BethanysPieShopDbContextConnection"]);
+});
+
+var app = builder.Build();
+
+//app.MapGet("/", () => "Hello World!");
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+
+app.UseStaticFiles();
+app.UseSession();
+
+//app.MapDefaultControllerRoute();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.MapRazorPages();
+app.MapBlazorHub();
+
+app.MapFallbackToPage("/app/{*catchall}", "/App/Index");
+
+
+DbInitializer.Seed(app);
+
+app.Run();
